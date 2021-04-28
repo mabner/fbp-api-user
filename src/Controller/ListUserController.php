8dcd37e085996bdbf335cf93e38db09394893b16
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-28 01:44:02 -0300

    ListUserController

--- src/Controller/ListUserController.php
+++ src/Controller/ListUserController.php
@@ -3,18 +3,30 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 class ListUserController
 {
-	public function __construct()
-	{
+	public function __construct(
+		private EntityManagerInterface $entityManager,
+		private SerializerInterface $serializer
+	) {
 	}
 
 	#[Route("/users", methods: ["GET"])]
-	public function __invoke()
+	public function __invoke(): Response
 	{
 		// TODO: Implement __invoke() method.
+
+		$repository = $this->entityManager->getRepository(User::class);
+		$users = $repository->findAll();
+		$response = $this->serializer->serialize($users, 'json');
+
+		return JsonResponse::fromJsonString($response);
 	}
 }
