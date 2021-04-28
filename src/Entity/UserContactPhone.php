d98d2c7be0f7edba77c33f5b89ddecd1f27301bc
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-28 02:19:56 -0300

    user contact getters and setters

--- src/Entity/UserContactPhone.php
+++ src/Entity/UserContactPhone.php
@@ -52,4 +52,124 @@ class UserContactPhone
     private ?\DateTime $updated_at = null;
 
 
+
+    /**
+     * Get the value of id
+     *
+     * @return ?int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param ?int $id
+     *
+     * @return self
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of area_code
+     *
+     * @return int
+     */
+    public function getAreaCode(): int
+    {
+        return $this->area_code;
+    }
+
+    /**
+     * Set the value of area_code
+     *
+     * @param int $area_code
+     *
+     * @return self
+     */
+    public function setAreaCode(int $area_code): self
+    {
+        $this->area_code = $area_code;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of phone_number
+     *
+     * @return string
+     */
+    public function getPhoneNumber(): string
+    {
+        return $this->phone_number;
+    }
+
+    /**
+     * Set the value of phone_number
+     *
+     * @param string $phone_number
+     *
+     * @return self
+     */
+    public function setPhoneNumber(string $phone_number): self
+    {
+        $this->phone_number = $phone_number;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set the value of created_at
+     *
+     * @param \DateTime $created_at
+     *
+     * @return self
+     */
+    public function setCreatedAt(\DateTime $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updated_at
+     *
+     * @return ?\DateTime
+     */
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set the value of updated_at
+     *
+     * @param ?\DateTime $updated_at
+     *
+     * @return self
+     */
+    public function setUpdatedAt(?\DateTime $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
 }
