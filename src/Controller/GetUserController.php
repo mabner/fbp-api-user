8f26e91ea843b44f2a9882daffddcc2a4664ec73
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-29 02:22:02 -0300

    GetUserController implemented

--- src/Controller/GetUserController.php
+++ src/Controller/GetUserController.php
@@ -3,18 +3,34 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 class GetUserController
 {
-	public function __construct()
-	{
+	public function __construct(
+		private EntityManagerInterface $entityManager,
+		private SerializerInterface $serializer
+	) {
 	}
 
 	#[Route("/users/{id}", name: "get_user", methods: ["GET"])]
-	public function __invoke()
+	public function __invoke(int $id): Response
 	{
-		// TODO: Implement __invoke() method.
+		// TODO: Presenting 'circular reference has been detected' error.
+		$repository = $this->entityManager->getRepository(User::class);
+		$user = $repository->find($id);
+		if (null === $user) {
+			return new JsonResponse(
+				['error' => 'User not found in the registry.'],
+				Response::HTTP_NOT_FOUND
+			);
+		}
+		return JsonResponse::fromJsonString($this->serializer->serialize($user, 'json'));
 	}
 }
