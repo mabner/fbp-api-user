9985ef6d2ecdd12e48f17c5ed85df964f6950ce3
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-30 04:45:44 -0300

    TestDocDB

---
+++ tests/TestDocDB.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Entity\User;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\ORM\Tools\ToolsException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TestDocDB extends WebTestCase
+{
+    protected EntityManagerInterface $eMI;
+    protected KernelBrowser $client;
+
+    protected function start(): void
+    {
+        $this->client = self::createClient();
+        $this->eMI = self::$kernel->getContainer()->get('doctrine')->getManager();
+        $sgdb = new SchemaTool($this->eMI);
+        $data = $this->eMI->getClassMetadata(User::class);
+        $sgdb->dropDatabase();
+
+        try {
+            $sgdb->createSchema([$data]);
+        } catch (ToolsException $error) {
+            $this->fail('Could not create the database. Error: "' . $error->getMessage() . '"');
+        }
+    }
+}
